6a71ebc87b27b2ab0c97ab7f2ab258307e718d2d
jalajSinha56 <jalajsinha140903@gmail.com>
2025-05-16 01:24:30 +0530

    helper file created

---
+++ main.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import preprocessor
+import helper
+
+st.sidebar.title("Whatsapp Chat Analyzer")
+
+uploaded_file = st.sidebar.file_uploader("Choose a file")
+if uploaded_file is not None:
+    # To read file as bytes:
+    bytes_data = uploaded_file.getvalue()
+    data = bytes_data.decode("utf-8")
+    df = preprocessor.preprocess(data)
+
+    st.dataframe(df)
+
+    user_list = df['user'].unique().tolist()
+    user_list.remove('group_notification')
+    user_list.sort()
+    user_list.insert(0,"Overall")
+    selected_user = st.sidebar.selectbox("Show analysis with respect to", user_list)
+
+    if st.sidebar.button("Show Analysis"):
+
+        num_messages = helper.fetch_stats(selected_user, df)
+        
+        col1, col2, col3, col4 = st.columns(4)
+
+        with col1:
+            st.header("Total Messages")
+            st.title(num_messages)
+            
