45ee6e1d75061352549ee4e7011f72246ec581dd
jalajSinha56 <jalajsinha140903@gmail.com>
2025-05-18 12:27:35 +0530

    Work done

--- helper.py
+++ helper.py
@@ -1,3 +1,15 @@
 def fetch_stats(selected_user, df):
-    if selected_user != 'Overall':
-        return df.shape[0]
+    if selected_user == 'Overall':
+        #1. fetch number of messages
+        num_messages = df.shape[0]
+        #2. fetch number of words
+        words = []
+        for message in df['message']:
+            words.extend(message.split())
+        return num_messages,words
+    else:
+        new_df = df[df['user'] == selected_user]
+        words=[]
+        for message in new_df['message']:
+            words.extend(message.split())
+        return num_messages, words
