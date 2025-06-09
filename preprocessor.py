1081c7e26b4e8bd5579306ccaff58cdb86db8798
jalajSinha56 <jalajsinha140903@gmail.com>
2025-06-09 09:53:51 +0530

    All work done

--- preprocessor.py
+++ preprocessor.py
@@ -2,21 +2,22 @@ import re
 import pandas as pd
 
 def preprocess(data):
-    pattern='\d{1,2}/\d{1,2}/\d{2,4},\s\d{1,2}:\d{2}\s-\s'
+    pattern = r'\d{1,2}/\d{1,2}/\d{2,4},\s\d{1,2}:\d{2}\s-\s'
     messages = re.split(pattern, data)[1:]
     dates = re.findall(pattern, data)
 
-    df = pd.DataFrame({'user_message':messages, 'message_date': dates})
-    #convert message_date type
-    df['message_date'] = pd.to_datetime(df['message_date'], format = '%d/%m/%Y, %H:%M - ')
-    df.rename(columns = {'message_date' : 'date'}, inplace = True)
+    df = pd.DataFrame({'user_message': messages, 'message_date': dates})
+
+    # Convert message_date to datetime
+    df['message_date'] = pd.to_datetime(df['message_date'], format='%d/%m/%Y, %H:%M - ')
+    df.rename(columns={'message_date': 'date'}, inplace=True)
 
     users = []
     messages = []
 
     for message in df['user_message']:
         entry = re.split('([\w\W]+?):\s', message)
-        if entry[1:]:  # user name
+        if entry[1:]:
             users.append(entry[1])
             messages.append(" ".join(entry[2:]))
         else:
@@ -27,10 +28,15 @@ def preprocess(data):
     df['message'] = messages
     df.drop(columns=['user_message'], inplace=True)
 
+    # Add all required columns
     df['year'] = df['date'].dt.year
     df['month'] = df['date'].dt.month_name()
+    df['month_num'] = df['date'].dt.month
     df['day'] = df['date'].dt.day
     df['hour'] = df['date'].dt.hour
     df['minute'] = df['date'].dt.minute
+    df['only_date'] = df['date'].dt.date
+    df['day_name'] = df['date'].dt.day_name()
+    df['period'] = df['hour'].apply(lambda x: f"{x}-{x+1}" if x != 23 else "23-00")
 
     return df
